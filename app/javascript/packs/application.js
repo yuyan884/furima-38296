14c869d72242c4543697c58952cc19d1eb00b7aa
YuyaHayashiBr <yuya.hayashi.br@gmail.com>
2022-08-17 17:29:58 +0900

    turbolinksの削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+//require("turbolinks").start() // JavaScriptの処理が正常に動作するようにturbolinksを削除
 require("@rails/activestorage").start()
 require("channels")
 
