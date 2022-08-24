1e6c3c6b2d190aa32d8b13dcaf72fa998a51d39b
YuyaHayashiBr <yuya.hayashi.br@gmail.com>
2022-08-24 17:41:56 +0900

    【不良】[ユーザ管理機能]苗字が漢字二文字以上だと登録できない 対応
    Issue#17
    https://github.com/YuyaHayashiBr/furima-38296/issues/17

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Password is invalid. Include both letters and numbers'}
-  validates :last_name,       format: { with: /\A[ぁ-んァ-ヶ一-龥々]\z/, message: 'Input full-width characters'}
-  validates :first_name,      format: { with: /\A[ぁ-んァ-ヶ一-龥々]\z/, message: 'Input full-width characters'}
+  validates :last_name,       format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Input full-width characters'}
+  validates :first_name,      format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Input full-width characters'}
   validates :last_name_kana,  format: { with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters'}
   validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters'}
   validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters'}
