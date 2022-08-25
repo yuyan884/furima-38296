c80145b97563a2198f7c70adb24b5b7277940917
YuyaHayashiBr <yuya.hayashi.br@gmail.com>
2022-08-25 11:45:43 +0900

    【不良】[ユーザ管理機能]誕生日のバリデーションが不足 対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Password is invalid. Include both letters and numbers'}
-  validates :last_name,       format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Input full-width characters'}
-  validates :first_name,      format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Input full-width characters'}
-  validates :last_name_kana,  format: { with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters'}
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters'}
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters'}
-  validates :birthday,        format: { with: /\A\d{4}[-]\d{2}[-]\d{2}\z/, message: "can't be blank"}
+  VALID_YEAR_REGEX = /\A19[3-9][0-9][-]\d{2}[-]\d{2}|#{(Time.now.year-5).to_s[0]}#{(Time.now.year-5).to_s[1]}[0-#{(Time.now.year-5).to_s[2]}][0-#{(Time.now.year-5).to_s[3]}][-]\d{2}[-]\d{2}\z/
+
+  validates :nickname,        presence: true
+  validates :password,                        format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Password is invalid. Include both letters and numbers'}
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Input full-width characters'}
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Input full-width characters'}
+  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters'}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters'}
+  validates :birthday,        presence: true, format: { with: VALID_YEAR_REGEX, message: "Input 1930-01-01 to #{Time.now.year-5}-12-31"}
 end
