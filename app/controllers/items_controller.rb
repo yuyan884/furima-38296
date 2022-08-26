dd758efa79e126fe6a9f60d7b26b6d5f02d51b6e
YuyaHayashiBr <yuya.hayashi.br@gmail.com>
2022-08-26 15:20:35 +0900

    商品出品機能 実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      binding.pry
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :detail, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :days_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  # Definitions
+  include ValidationDefinition
+
+  # Association
+  belongs_to       :user          # Userモデル
+  has_one_attached :image         # ActiveStorage
+  belongs_to       :category      # ActiveHash
+  belongs_to       :condition     # ActiveHash
+  belongs_to       :shipping_fee  # ActiveHash
+  belongs_to       :prefecture    # ActiveHash
+  belongs_to       :days_delivery # ActiveHash
+
+  # Validation
+  validates :name,             presence: true, length: {maximum: MAX_NAME_LENGTH   }
+  validates :detail,           presence: true, length: {maximum: MAX_DETAIL_LENGTH }
+  validates :category_id,      numericality: { greater_than: BLANK_MENU, less_than: Category.data.length,     message: "can't be blank" }
+  validates :condition_id,     numericality: { greater_than: BLANK_MENU, less_than: Condition.data.length,    message: "can't be blank" }
+  validates :shipping_fee_id,  numericality: { greater_than: BLANK_MENU, less_than: ShippingFee.data.length,  message: "can't be blank" }
+  validates :prefecture_id,    numericality: { greater_than: BLANK_MENU, less_than: Prefecture.data.length,   message: "can't be blank" }
+  validates :days_delivery_id, numericality: { greater_than: BLANK_MENU, less_than: DaysDelivery.data.length, message: "can't be blank" }
+  validates :price,            numericality: { greater_than_or_equal_to: (MIN_PRICE+1), less_than_or_equal_to: (MAX_PRICE+1), message: "is out of setting range" }, format: { with: HALF_INT_REGEX, message: "is invalid. Input half-width characters"}
 end
