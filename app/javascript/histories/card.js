211735b84286b9b0f2d1e8e0e4af12a6c8add438
YuyaHayashiBr <yuya.hayashi.br@gmail.com>
2022-09-07 10:49:49 +0900

    【レビュー指摘】[商品購入機能]公開鍵、秘密鍵を環境変数に置き換えていない
    Issue #41

--- app/javascript/histories/card.js
+++ app/javascript/histories/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_e5955ff413f78b8a0412fe90"); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
